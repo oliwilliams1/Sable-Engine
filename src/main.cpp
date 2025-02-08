7f86dc1440ae33888b40919acd2842b933fa82bc
Oli Williams <imhungryoliver@gmail.com>
2025-02-08 19:01:26 +1300

    Add libraries

--- src/main.cpp
+++ src/main.cpp
@@ -1,7 +1,22 @@
 #include <stdio.h>
+#include <glm/glm.hpp>
+#include <GL/glew.h>
+#include <GLFW/glfw3.h>
+#include <stb_image.h>
+#include <assimp/Importer.hpp>
+#include <imgui.h>
+#include <ImGuiFileDialog.h>
 
 int main(char argc, char* argv[])
 {
-    printf("Hello World!\n");
+    glm::vec3 x = glm::vec3(1.0f);
+    x.x += 1.0f;
+
+    GLuint someValue;
+	someValue = 1;
+    someValue += 1;
+
+
+    printf("Hello World! \t %f \t %i \n", x.x, someValue);
     return 0;
 }
